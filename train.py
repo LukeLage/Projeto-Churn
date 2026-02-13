28fd7197d700732059d3f0e65072ed60a30a4986
Luke Lage <lukelage646@gmail.com>
2026-02-13 07:38:53 -0300

    feat: modifying data and creating the model

--- train.py
+++ train.py
@@ -70,4 +70,70 @@ feature_importance = (pd.Series(arvore.feature_importances_, index= X_train.colu
                         )
 
 feature_importance['acum'] = feature_importance[0].cumsum()
-feature_importance[feature_importance[0]> 0.01]
+feature_importance[feature_importance[0] < 0.96]
+
+# %%
+
+best_features = feature_importance[feature_importance[0] < 0.96]['index'].tolist()
+best_features
+
+# %%
+# MODIFY 
+
+from feature_engine import discretisation
+
+tree_discretisation = discretisation.DecisionTreeDiscretiser(variables= best_features,
+                                                            regression= False,
+                                                            bin_output= 'bin_number',
+                                                            cv = 3)
+
+tree_discretisation.fit(X_train[best_features], y_train)
+
+# %%
+
+X_train_transform = tree_discretisation.transform(X_train[best_features])
+X_train_transform
+
+# %%
+# MODEL
+
+from sklearn import linear_model
+
+reg = linear_model.LogisticRegression(penalty= None, random_state= 42, max_iter= 100000)
+reg.fit(X_train_transform, y_train)
+
+# %%
+
+from sklearn import metrics
+
+y_train_predict = reg.predict(X_train_transform)
+y_train_proba = reg.predict_proba(X_train_transform)[:, 1]
+
+acc_train = metrics.accuracy_score(y_train, y_train_predict)
+auc_train = metrics.roc_auc_score(y_train, y_train_proba)
+print('Acurácia de Treino: ', acc_train)
+print('AUC de Treino: ', auc_train)
+
+# %%
+
+X_test_transform = tree_discretisation.transform(X_test[best_features])
+
+y_test_predict = reg.predict(X_test_transform)
+y_test_proba = reg.predict_proba(X_test_transform)[:, 1]
+
+acc_test = metrics.accuracy_score(y_test, y_test_predict)
+auc_test = metrics.roc_auc_score(y_test, y_test_proba)
+print('Acurácia de Teste: ', acc_test)
+print('AUC de Teste: ', auc_test)
+
+# %% 
+
+oot_transform = tree_discretisation.transform(oot[best_features])
+
+y_oot_predict = reg.predict(oot_transform)
+y_oot_proba = reg.predict_proba(oot_transform)[:, 1]
+
+acc_oot = metrics.accuracy_score(oot[target], y_oot_predict)
+auc_oot = metrics.roc_auc_score(oot[target], y_oot_proba)
+print('Acurácia de OOT: ', acc_oot)
+print('AUC de OOT: ', auc_oot)
