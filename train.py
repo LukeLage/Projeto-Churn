cab860a36ea753565fb75f60e0676ea139e0d3f5
Luke Lage <lukelage646@gmail.com>
2026-02-17 18:03:47 -0300

    feat: model assess

--- train.py
+++ train.py
@@ -96,17 +96,18 @@ onehot = encoding.OneHotEncoder(variables= best_features, ignore_format= True)
 
 from sklearn import linear_model
 
-reg = linear_model.LogisticRegression(penalty= None, random_state= 42, max_iter= 100000)
+model = linear_model.LogisticRegression(penalty= None, random_state= 42, max_iter= 100000)
 
 model_pipeline = pipeline.Pipeline(
     steps= [
         ('Discretizar', tree_discretisation),
         ('OneHot', onehot),
-        ('Model', reg)
+        ('Model', model)
     ]
 )
 
 model_pipeline.fit (X_train, y_train)
+
 # %%
 
 from sklearn import metrics
@@ -116,6 +117,9 @@ y_train_proba = model_pipeline.predict_proba(X_train)[:, 1]
 
 acc_train = metrics.accuracy_score(y_train, y_train_predict)
 auc_train = metrics.roc_auc_score(y_train, y_train_proba)
+
+roc_train = metrics.roc_curve(y_train, y_train_proba)
+
 print('Acurácia de Treino: ', acc_train)
 print('AUC de Treino: ', auc_train)
 
@@ -126,6 +130,9 @@ y_test_proba = model_pipeline.predict_proba(X_test)[:, 1]
 
 acc_test = metrics.accuracy_score(y_test, y_test_predict)
 auc_test = metrics.roc_auc_score(y_test, y_test_proba)
+
+roc_test = metrics.roc_curve(y_test, y_test_proba)
+
 print('Acurácia de Teste: ', acc_test)
 print('AUC de Teste: ', auc_test)
 
@@ -136,5 +143,29 @@ y_oot_proba = model_pipeline.predict_proba(oot[features])[:, 1]
 
 acc_oot = metrics.accuracy_score(oot[target], y_oot_predict)
 auc_oot = metrics.roc_auc_score(oot[target], y_oot_proba)
+
+roc_oot = metrics.roc_curve(oot[target], y_oot_proba)
+
 print('Acurácia de OOT: ', acc_oot)
 print('AUC de OOT: ', auc_oot)
+
+
+# %%
+
+plt.figure(dpi= 400)
+plt.plot(roc_train[0], roc_train[1], label= 'Treino')
+plt.plot(roc_test[0], roc_test[1], label= 'Teste')
+plt.plot(roc_oot[0], roc_oot[1], label= 'OOT')
+plt.plot([0, 1], [0, 1], '--', color= 'grey')
+plt.grid(True)
+plt.ylabel ('Sensibilidade')
+plt.xlabel ('1 - Especificidade')
+plt.title('Curva ROC')
+plt.legend(
+    [f'Treino: :{100*auc_train: .2f}',
+    f'Teste: :{100*auc_test: .2f}',
+    f'Out-Of-Time: :{100*auc_oot: .2f}']
+)
+
+# %% 
+
